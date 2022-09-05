1f27e8186746eaa3d07aec4ce209e567e23ef67d
verah <verahnyagaka101@gmail.com>
2022-09-05 22:31:34 +0300

    updated models

--- app/backend/db/migrate/20220905185623_create_poems.rb
+++ app/backend/db/migrate/20220905185623_create_poems.rb
@@ -1,4 +1,12 @@
 class CreatePoems < ActiveRecord::Migration[6.1]
   def change
+    create_table :poems do |t|
+      t.string :title
+      t.text :body
+      t.string :category
+      t.integer :authors_id
+      t.integer :categories_id
+      t.timestamps
+    end
   end
 end
--- app/backend/db/migrate/20220905185637_create_inspirations.rb
+++ app/backend/db/migrate/20220905185637_create_inspirations.rb
@@ -1,4 +1,12 @@
 class CreateInspirations < ActiveRecord::Migration[6.1]
   def change
+    create_table :inspirations do |t|
+      t.string :title
+      t.text :body
+      t.string :category
+      t.integer :authors_id
+      t.integer :categories_id
+      t.timestamps
+    end
   end
 end
