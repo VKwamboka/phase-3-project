e401a10cf2491fd4f7408c6b16d6927c33fb6586
verah <verahnyagaka101@gmail.com>
2022-09-06 22:02:21 +0300

    seeded data

--- app/backend/app/controllers/application_controller.rb
+++ app/backend/app/controllers/application_controller.rb
@@ -6,4 +6,40 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  # ALL POEMS
+  get '/poems' do
+    poems = Poem.all
+    poems.to_json
+  end
+
+  # ALL POEM AUTHORS
+  get '/poemauthors' do
+    poemauthors = Author.all
+    poemauthors.to_json
+  end
+
+  # All POEM CATEGORIES
+  get '/poemcategories' do
+    poemcategories = Category.all
+    poemcategories.to_json
+  end
+
+  # ALL INSPIRATIONS
+  get '/inspirations' do
+    inspirations = Inspiration.all
+    inspirations.to_json
+  end
+
+  # ALL INSPIRATIONS AUTHORS
+  get '/inspirationauthors' do
+    inspirationauthors = InspirationAuthor.all
+    inspirationauthors.to_json
+  end
+
+  # All INSPIRATIONS CATEGORIES
+  get '/inspirationcategories' do
+    inspirationcategories = InspirationCategory.all
+    inspirationcategories.to_json
+  end
+
 end
